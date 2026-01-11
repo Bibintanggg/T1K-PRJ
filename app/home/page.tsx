bb7a3c196b240c8584ca81f011cb9a69a49e3606
Bibintanggg <bintangyudhaa8@gmail.com>
2026-01-11 19:16:35 +0700

    feat: add new component "Count UP for increment days"and refactoring text

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,6 +7,9 @@ import logo from "@/public/Images/logo.png";
 import ScrollReveal from '@/components/ScrollReveal';
 import { Gamepad, HomeIcon } from 'lucide-react';
 import Dock from '@/components/Dock';
+import { motion, useInView } from "motion/react";
+import CountUp from "@/components/CountUp";
+
 
 export default function Home() {
     const scrollContainerRef = useRef<HTMLDivElement>(null);
@@ -15,6 +18,13 @@ export default function Home() {
         { icon: <HomeIcon size={18} />, label: 'Home', onClick: () => alert('Home!') },
         { icon: <Gamepad size={18} />, label: 'Games', onClick: () => alert('Archive!') },
     ];
+
+    const sectionRef = useRef<HTMLDivElement>(null);
+    const inView = useInView(sectionRef, {
+        once: true,
+        margin: "-20% 0px",
+    });
+
     return (
         <div
             className="bg-black min-h-screen overflow-auto"
@@ -52,12 +62,11 @@ export default function Home() {
                             containerClassName="text-center"
                             textClassName="text-white text-2xl md:text-3xl"
                         >
-                            This is another text to test the scroll reveal animation.
-                            Scroll down to see more effects!
+                            Helloo, tikk!!
                         </ScrollReveal>
                     </div>
                 </section>
-                
+
                 <section className="min-h-screen w-full flex items-center justify-center px-4">
                     <div className="max-w-4xl">
                         <ScrollReveal
@@ -68,11 +77,59 @@ export default function Home() {
                             containerClassName="text-center"
                             textClassName="text-white text-2xl md:text-3xl"
                         >
-                            This is another text to test the scroll reveal animation.
-                            Scroll down to see more effects!
+                            I just want to say about,  hmmm...
                         </ScrollReveal>
                     </div>
                 </section>
+
+                <section
+                    ref={sectionRef}
+                    className="min-h-screen w-full flex items-center justify-center px-4"
+                >
+                    <div className="max-w-4xl text-center space-y-6">
+
+                        {/* Text */}
+                        <ScrollReveal
+                            scrollContainerRef={scrollContainerRef}
+                            enableBlur
+                            baseOpacity={0.1}
+                            blurStrength={8}
+                            textClassName="text-white text-2xl md:text-3xl"
+                        >
+                            Maybe, after
+                        </ScrollReveal>
+
+                        {/* CountUp + Blur */}
+                        <motion.div
+                            initial={{ opacity: 0, filter: "blur(8px)" }}
+                            animate={inView ? { opacity: 1, filter: "blur(0px)" } : {}}
+                            transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+                            className="flex items-center gap-5 justify-center"
+                        >
+                            <CountUp
+                                from={0}
+                                to={152}
+                                duration={1.5}
+                                startWhen={inView}
+                                className="text-white text-5xl font-bold"
+                            />
+
+                            <p className="text-white text-3xl">Days</p>
+                        </motion.div>
+
+                         <ScrollReveal
+                            scrollContainerRef={scrollContainerRef}
+                            enableBlur
+                            baseOpacity={0.1}
+                            blurStrength={8}
+                            textClassName="text-white text-2xl md:text-3xl"
+                        >
+                            Gue gabisa bilang ini ke lu, but.. maybe this is the good time buat bilang
+                        </ScrollReveal>
+
+                    </div>
+                </section>
+
 
                 {/* Section 4 - Final spacer */}
                 <section className="h-screen w-full flex items-center justify-center">
